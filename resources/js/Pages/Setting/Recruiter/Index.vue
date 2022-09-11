f49476c82803401d266ee3b300c57c6d863c9a4f
Sergey Vlsdislavovich <stalkerdp500@gmail.com>
2022-09-11 21:44:36 +0300

    change recruiters page

--- resources/js/Pages/Setting/Recruiter/Index.vue
+++ resources/js/Pages/Setting/Recruiter/Index.vue
@@ -107,26 +107,25 @@ function serched (input) {
                             d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z" />
                     </svg>
 
-                    <h3 class="text-xl">{{ recruiter.name }} </h3>
+                    <h3 class="text-xl font-bold">{{ recruiter.name }} </h3>
                 </div>
                 <div
-                    class=" py-4 border-t-2 border-systems-700/50   w-full gap-2  flex flex-col items-center md:justify-center justify-evenly flex-wrap">
-                    <div class="w-full">
-                        <h3 class="flex-1 ">Команда</h3>
-                        <div class="  justify-center flex items-center w-11/12">
+                    class=" py-4 border-t-2 border-systems-700/50   w-full gap-2  flex  items-center md:justify-start justify-evenly flex-wrap">
+                    <div class=" w-full flex bg-systems-300 items-center px-2 rounded-md">
+                        <h3 class="font-semibold ">В команде</h3>
+                        <div class="flex items-center m-2 md:w-1/2">
                             <VueMultiselect @update:model-value="recruiterTeamUpdate($event, recruiter)"
                                 :multiple="false" selectLabel="Изменить команду на эту" v-model="recruiter.team"
                                 :options="props.teamsList" label="name" :searchable="true" placeholder="выбор команды">
                             </VueMultiselect>
                         </div>
                     </div>
-                    <div class="w-full">
-                        <h3 class="flex-1 my-2 ">Владелец</h3>
-                        <div class="   justify-center flex items-center w-11/12">
+                    <div class="w-full flex bg-systems-300 items-center px-2 rounded-md">
+                        <h3 class=" font-semibold">Владелец</h3>
+                        <div class="flex items-center m-2 md:w-1/2">
                             <VueMultiselect @update:model-value="recruiterOwnerUpdate($event, recruiter)"
-                                :multiple="false" selectLabel="Назначить владельцем рекрутера" track-by="name"
-                                deselectLabel="убрать из этой команды" v-model="recruiter.owner"
-                                :options="props.userList" label="name" :searchable="true"
+                                :multiple="false" selectLabel="" track-by="name" deselectLabel="убрать из этой команды"
+                                v-model="recruiter.owner" :options="props.userList" label="name" :searchable="true"
                                 placeholder="выбор пользователя">
                             </VueMultiselect>
                         </div>
