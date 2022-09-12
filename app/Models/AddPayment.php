666c6833ac94a43b14aa874013606763c0f1c793
Sergey Vlsdislavovich <stalkerdp500@gmail.com>
2022-09-12 11:23:28 +0300

    fix add payments

--- app/Models/AddPayment.php
+++ app/Models/AddPayment.php
@@ -22,10 +22,19 @@ class AddPayment extends Model
             $query->where('add_payments.year', $filters['year']);
             $query->where('add_payments.month', $filters['month']);
         } else {
-            $query->whereRaw(
-                "month = (select max(`month`) from payments where year = (select max(`year`) from payments))
-                 AND year = (select max(`year`) from payments)"
-            );
+            $query
+                ->where('add_payments.month', '=', function ($query) {
+                    $query->selectRaw('max(month)')->where('p.year', '=', function ($query) {
+                        $query->selectRaw('max(p.year)')->from('payments as p');
+                    })->from('payments as p');
+                })
+                ->where('add_payments.year', '=', function ($query) {
+                    $query->selectRaw('max(p.year)')->from('payments as p');
+                });
+            // $query->whereRaw(
+            //     "month = (select max(`month`) from payments where year = (select max(`year`) from payments))
+            //      AND year = (select max(`year`) from payments)"
+            // );
         }
     }
 }
