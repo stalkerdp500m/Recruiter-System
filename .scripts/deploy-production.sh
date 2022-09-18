82e58b9f57ece02be446705bd67c51fc1d78a358
Sergey Vlsdislavovich <stalkerdp500@gmail.com>
2022-09-18 13:12:00 +0300

    add deploy production

---
+++ .scripts/deploy-production.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+# Enter maintenance mode or return true
+# if already is in maintenance mode
+(php artisan down) || true
+
+# Pull the latest version of the app
+git pull origin main
+
+# Install composer dependencies
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+# Clear the old cache
+php artisan clear-compiled
+
+# Recreate cache
+php artisan optimize
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Install dev dependencies
+npm install
+
+# Compile npm assets
+npm run prod
+
+# Run database migrations
+php artisan migrate --force
+
+# Exit maintenance mode
+php artisan up
+
+pm2 restart  worker/pm2-laravel-queue.yml
+
+echo "Deployment finished!"
