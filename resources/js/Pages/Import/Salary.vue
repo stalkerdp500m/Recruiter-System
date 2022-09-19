10c1f5a12d9f2eb0bf9f634a99571774e789c443
Sergey Vlsdislavovich <stalkerdp500@gmail.com>
2022-09-19 23:44:26 +0300

    fix typos in salary import page

--- resources/js/Pages/Import/Salary.vue
+++ resources/js/Pages/Import/Salary.vue
@@ -106,8 +106,8 @@ let raw = JSON.stringify({
   "data": [
     {
       "salary": "сумма (double)",
-      "year": "год выплаты (namber)",
-      "month": "месц выплаты (namber)",
+      "year": "год выплаты (number)",
+      "month": "месц выплаты (number)",
       "client_name": "имя и фамилия клиента (string)",
       "client_pasport": "номер паспорта клиента (string)",
       "project": "навзвание проекта (string)",
